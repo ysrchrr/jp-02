cb53cf25692a4d5b302a7155f8d96b79f6d99109
Muhammad Yasir Choiri <34145256+ysrchrr@users.noreply.github.com>
2023-07-24 19:48:43 +0700

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Dashboard",
+    page_icon="🏠",
+)
+
+st.title('Selamat datang👋')
+st.subheader('Website Analis Sentimen Ulasan Tokopedia dengan Metode Naive Bayes 📊')
+
+st.markdown(
+    """
+    ### Klasifikasi Naive Bayes
+    Naive Bayes classifier merupakan salah satu metoda pemelajaran mesin yang memanfaatkan perhitungan probabilitas dan statistik yang dikemukakan oleh ilmuwan Inggris Thomas Bayes, yaitu memprediksi probabilitas pada masa depan berdasarkan pengalaman pada masa sebelumnya.
+    [Wikipedia](https://id.wikipedia.org/wiki/Naive_Bayes_classifier)
+    ### Tokopedia
+    PT Tokopedia merupakan perusahaan perdagangan elektronik atau sering disebut toko daring. Sejak didirikan pada tahun 2009, Tokopedia telah bertransformasi menjadi sebuah unicorn yang berpengaruh tidak hanya di Indonesia tetapi juga di Asia Tenggara.
+    [Wikipedia](https://id.wikipedia.org/wiki/Tokopedia)
+"""
+)
