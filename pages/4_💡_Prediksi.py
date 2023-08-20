e87f0709564a539796de90713d5adabec30f8d83
ysrchrr <ysrchr@gmail.com>
2023-08-20 17:52:07 +0700

    file from client

--- pages/4_💡_Prediksi.py
+++ pages/4_💡_Prediksi.py
@@ -1,42 +1,120 @@
 import streamlit as st
-import pandas as pd
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.feature_extraction.text import CountVectorizer
+import re
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 
-# Buat aplikasi Streamlit
-def predict_sentiment(text):
-    text_vectorized = vectorizer.transform([text])
-    prediction = naive_bayes.predict(text_vectorized)
-    return prediction[0]
+st.set_page_config(
+    page_title="Prediksi",
+    page_icon="💡",
+)
 
-st.title('Prediksi')
-# Baca file CSV dari direktori lokal
-file_path = 'https://muyacho.com/documents/dataset_postprocessing.csv'  # Ganti dengan path ke file CSV Anda
-# file_path = 'E:\PJ\skripsi-bagas\coba.csv'  # Ganti dengan path ke file CSV Anda
-data_df = pd.read_csv(file_path)
+# Menginisialisasi Sastrawi Stemmer
+factory = StemmerFactory()
+stemmer = factory.create_stemmer()
 
-# Pisahkan data menjadi data ulasan dan label
-responses = data_df['response'].tolist()
-labels = data_df['label'].tolist()
+# Mengunduh kamus stopword Bahasa Indonesia dari NLTK
+nltk.download('stopwords')
+stop_words = set(stopwords.words('indonesian'))
+
+# Membuat fungsi untuk membersihkan teks
+def clean_text(text):
+    # Mengubah ke lowercase
+    text = text.lower()
+
+    # Menghapus karakter non-alfanumerik
+    text = re.sub(r'\W+', ' ', text)
+
+    # Menghapus angka
+    text = re.sub(r'\d+', '', text)
+
+    return text
+
+def tokenizing(text):
+    tokens = word_tokenize(str(text))
+    return tokens
+
+def stopword_removal(tokens):
+    stopword_tokens = [token for token in tokens if token not in stop_words]
+    return stopword_tokens
+
+def stem_tokens(stopword_tokens):
+    stemmed_tokens = [stemmer.stem(token) for token in stopword_tokens]
+    return stemmed_tokens
 
 
+def load_sentiment_dictionary():
+    sentiment_dict = {
+        'positif': set(),
+        'negatif': set()
+    }
 
-# Proses data teks menjadi vektor fitur menggunakan Count Vectorizer
-vectorizer = CountVectorizer()
-X_vectorized = vectorizer.fit_transform(responses)
+    # Membaca file kamus positif
+    with open('positive.txt', 'r', encoding='utf-8') as f:
+        for line in f:
+            word = line.strip()
+            sentiment_dict['positif'].add(word)
 
-# Latih model Naive Bayes
-naive_bayes = MultinomialNB()
-naive_bayes.fit(X_vectorized, labels)
+    # Membaca file kamus negatif
+    with open('negative.txt', 'r', encoding='utf-8') as f:
+        for line in f:
+            word = line.strip()
+            sentiment_dict['negatif'].add(word)
 
-# Masukkan ulasan produk
-st.write('Komentar Aplikasi Tokopedia : ')
-user_input = st.text_area('Komentar')
+    return sentiment_dict
 
-if st.button('Prediksi'):
-    if user_input:
-        prediction = predict_sentiment(user_input)
-        st.write(f'Hasil Prediksi: {prediction}')
+
+def analyze_sentiment(text, sentiment_dict):
+    tokens = clean_text(text)
+    tokens = tokenizing(text)
+                
+    positive_count = sum(token in sentiment_dict['positif'] for token in tokens)
+    negative_count = sum(token in sentiment_dict['negatif'] for token in tokens)
+
+    if positive_count > negative_count:
+        sentiment = 'Positif'
+    elif positive_count < negative_count:
+        sentiment = 'Negatif'
     else:
-        st.write('Masukkan komentar terlebih dahulu')
+        sentiment = 'Netral'
+    
+    return sentiment
+
+# Mengatur tampilan Streamlit
+sentiment_dict = load_sentiment_dictionary()
+
+def main():
+    st.title("Prediksi")
+    st.write("Masukkan komentar Tokopedia di bawah ini untuk mengklasifikasi sentimen")
+    
+    text = st.text_area("Tulis Komentar :")
+    
+    if st.button("Analisis"):  
+
+        sentiment = analyze_sentiment(text, sentiment_dict)
+        st.write("Hasil Sentimen :", sentiment) 
+
+        st.write("Tahapan Proses Preprocessing") 
+
+        clean = clean_text(text)
+        st.text_input("#1. Hasil Cleaning :", clean)
+
+        tokens = tokenizing(clean)
+        st.text_input("#2. Hasil Tokenizing :", tokens)
+        
+        filtered_tokens = stopword_removal(tokens)
+        st.text_input("#3. Hasil Stopword Removal :", filtered_tokens)
+        
+        stemmed_tokens = stem_tokens(filtered_tokens)
+        st.text_input("#4. Hasil Stemming :", stemmed_tokens)
+
+        sentiment = analyze_sentiment(text, sentiment_dict)
+        st.text_input("#5. Kesimpulan Klasifikasi Analisis Sentimen :", sentiment)
+
+
+
+# Menjalankan aplikasi
+if __name__ == "__main__":
+    main()
 
